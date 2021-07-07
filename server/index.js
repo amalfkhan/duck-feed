f6eb5975b7325a40d22f0b1d3666e22de68cbbd6
Amal Khan <amalfkhan@gmail.com>
2021-07-07 13:35:05 -0700

    connected to db

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 
+const CONNECTION_URL =
+  "mongodb+srv://amalfkhan:amalfkhan23@cluster0.da06f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const port = process.env.PORT || 5000;
 const app = express();
-const port = 5000;
 
-app.get("/", (req, res) => {
-  res.json({ hello: "json" });
-});
+app.use(express.json());
+app.use(cors);
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`);
+    });
+  })
+  .catch((e) => {
+    console.error(e.message);
+  });
